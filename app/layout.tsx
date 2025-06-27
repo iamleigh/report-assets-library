ffb7111fbd1b07fe2b3459f3f163c9feb3b08090
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2025-06-27 12:29:49 -0500

    chore: Migrate global styles to SCSS
    - Update file ext to SCSS
    - Remove root styles
    - Update background and foreground colors to support tailwind palette (gray)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import '@fortawesome/fontawesome-svg-core/styles.css'
-import './globals.css'
+import './globals.scss'
 
 const geistSans = Geist({
     variable: '--font-geist-sans',

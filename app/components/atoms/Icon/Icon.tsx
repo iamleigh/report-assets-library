d323c898d12a1a4492ea8681649b2d3290106bf1
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2025-06-27 07:17:01 -0500

    feat(icon): Create atom component

---
+++ app/components/atoms/Icon/Icon.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { iconNameMap, iconSizeMap } from './icon.types'
+import type { IconName, IconSize } from './icon.types'
+
+interface IconProps {
+    name: IconName
+    size?: IconSize
+}
+
+const Icon: React.FC<IconProps> = ({ name, size = 'md' }) => {
+    return (
+        <FontAwesomeIcon
+            icon={iconNameMap[name.toLowerCase() as keyof typeof iconNameMap]}
+            size={iconSizeMap[size.toLowerCase() as keyof typeof iconSizeMap]}
+            data-testid="ral-icon"
+        />
+    )
+}
+
+export default Icon
---
+++ app/components/atoms/Icon/icon.types.ts
@@ -0,0 +1,17 @@
+import { faArrowUp, faAdd } from '@fortawesome/free-solid-svg-icons'
+import type { SizeProp } from '@fortawesome/fontawesome-svg-core'
+
+export const iconNameMap = {
+    up: faArrowUp,
+    add: faAdd,
+}
+
+export type IconName = keyof typeof iconNameMap
+
+export const iconSizeMap: Record<string, SizeProp> = {
+    sm: 'sm',
+    md: 'lg',
+    lg: '2x',
+}
+
+export type IconSize = keyof typeof iconSizeMap

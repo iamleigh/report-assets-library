3741749e8421fcb7b4fee94f647253c87462ac78
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2025-06-27 08:17:57 -0500

    feat(button): Update atom component styles
    - Use SCSS to assign component styles.
    - Install `clsx` package.

--- app/components/atoms/Button/Button.tsx
+++ app/components/atoms/Button/Button.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Icon from '@atom/Icon/Icon'
 import type { IconName, IconSize } from '@atom/Icon/icon.types'
+import styles from './Button.module.scss'
+import clsx from 'clsx'
 
 interface ButtonProps {
     label: string
@@ -12,18 +14,15 @@ interface ButtonProps {
 }
 
 const Button: React.FC<ButtonProps> = ({ label, icon, iconSize, disabled, testId, onClick, ...props }) => {
-    const baseStyles = 'bg-gray-400 text-white rounded-lg py-2 px-4 transition-all'
-    const hoverStyles = 'hover:bg-gray-600'
-
     return (
         <button
-            className={`${baseStyles} ${hoverStyles}`}
+            className={clsx(styles['ral-button'])}
             {...(disabled && { disabled: true })}
             data-testid={testId ? testId : 'ral-button'}
             onClick={onClick}
             {...props}>
-            {icon && <Icon name={icon} size={iconSize} />}
-            {label}
+            {icon && <Icon name={icon} size={iconSize ? iconSize : 'sm'} />}
+            <span className={clsx(styles['ral-button__label'])}>{label}</span>
         </button>
     )
 }

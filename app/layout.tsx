d7df6d4bbd09248ab9c48d65dd52734e2df08eaa
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2025-06-28 05:05:16 -0500

    feat(layout): Wrap main content with max width block

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
             <body
                 className={`${geistSans.variable} ${geistMono.variable} antialiased`}
             >
-                {children}
+                <div className="mx-auto max-w-4xl">{children}</div>
             </body>
         </html>
     )

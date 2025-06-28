5d82421cb3d7f4eeefbd06344651b3aa4437f773
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2025-06-28 03:09:25 -0500

    fix(icon): Enable additional class names

--- components/atoms/Icon/Icon.tsx
+++ components/atoms/Icon/Icon.tsx
@@ -6,13 +6,15 @@ import type { IconName, IconSize } from './icon.types'
 interface IconProps {
     name: IconName
     size?: IconSize
+    className?: string
 }
 
-const Icon: React.FC<IconProps> = ({ name, size = 'md' }) => {
+const Icon: React.FC<IconProps> = ({ name, size = 'md', className }) => {
     return (
         <FontAwesomeIcon
             icon={iconNameMap[name.toLowerCase() as keyof typeof iconNameMap]}
             size={iconSizeMap[size.toLowerCase() as keyof typeof iconSizeMap]}
+            className={className}
             data-testid="ral-test--icon"
         />
     )
